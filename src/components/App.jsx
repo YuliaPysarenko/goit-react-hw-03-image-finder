f88519e86a4c9f251bfad4da4ec5cd9953e2b4b0
YuliaPysarenko <y.pisarenko@ukr.net>
2023-10-20 15:49:06 +0300

    wk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import Searchbar from "./Searchbar";
 import ImageGallery from "./ImageGallery";
 import ImageGalleryItem from "./ImageGalleryItem";
 
-
+const IPA_KEY = `37860129-0a816fc38343337d9878906bd`;
 class App extends Component {
  
   state = {
@@ -11,21 +11,68 @@ class App extends Component {
     items: [],
     totalHits: 0,
   }
-    
+   
+  
+  componentDidUpdate(prevProps, prevState) {
+  
+    if (prevState.name !== this.state.name) {
+      this.setState({ loading: true })
+      fetch(`https://pixabay.com/api/?q=${this.state.name}&page=1&key=${IPA_KEY}&image_type=photo&orientation=horizontal&per_page=12`)
+        .then(response => {
+          if (response.ok) {
+            return response.json()
+          }
+          return Promise.reject(
+            new Error(`Нажаль картинок з вашим пошуком ${this.state.name}, не має`)
+          );
+        })
+        // .then(hits  => this.setState(prevState => ({ items: prevState.items, ...hits })))
+     .then(result  => this.setState({ items: result.hits }))
+        // .then(response => response.images)
+        //  .then(images => this.setState({ items: [...images] }))
+        // .then(images => this.setState({ items: [images] }))
+      
+        .catch(error => this.setState({error}))
+        .finally(() => this.setState({ loading: false })) 
+    }
+  }
 
-  handleFormSabmit = (nameForm, hits) => {
+
+
+  handleFormSabmit = (nameForm) => {
     this.setState({
       name: nameForm,
-      // items: hits,
+       items: [],
     })
   }
 
   render() {
+    const { items } = this.state;
+    const { loading } = this.state;
+    const { error } = this.state;
+    // const { name } = this.props;
+
     return (
+     
       <div>
+    
         <Searchbar onSubmit={this.handleFormSabmit} />
-        <ImageGallery name={this.state.name} hits={this.state.items } />
-        <ImageGalleryItem/>
+        <div>
+        {error && <div><p>{error.message}</p></div>}
+        {loading && <div>Loading</div>}
+          {items && <ul>
+            {items.map(({item}) =>
+              <li key={item.id}>
+              <img src={item.webformatURL}  large={item.largeImageURL} alt="" />
+            </li>
+           )}
+
+         
+          </ul>}
+        </div>
+        {/* <ImageGallery name={this.state.name} hits={this.state.items } />  */}
+        <ImageGallery/>
+        <ImageGalleryItem/> 
       </div>
     )
   }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,42 +1,40 @@
 import React, { Component } from "react";
 import css from './ImageGallery.module.css';
-import ImageGalleryItem from "components/ImageGalleryItem";
+// import ImageGalleryItem from "components/ImageGalleryItem";
 
-const IPA_KEY = `37860129-0a816fc38343337d9878906bd`;
+// const IPA_KEY = `37860129-0a816fc38343337d9878906bd`;
 
 class ImageGallery extends Component {
     state = {
-      items: [],
+      // items: [],
     
-      loading: false,
-      error: null,
+      // loading: false,
+      // error: null,
   
     }
 
-  componentDidUpdate(prevProps, prevState) {
+  // componentDidUpdate(prevProps, prevState) {
   
-    if (prevProps.name !== this.props.name) {
-     this.setState({loading:true})
-      fetch(`https://pixabay.com/api/?q=${this.props.name}&page=1&key=${IPA_KEY}&image_type=photo&orientation=horizontal&per_page=12`)
-        .then(response => {
-          if (response.ok) {
-            return response.json()
-          }
-          return Promise.reject(
-            new Error(`Нажаль картинок з вашим пошуком ${this.state.name}, не має`)
-          );
-        })
-        .then(images => this.setState(prevState => ({ items: prevState.items, ...images })))
-        // .then(response => response.images)
-        //  .then(images => this.setState({ items: [...images] }))
-        // .then(images => this.setState({ items: [images] }))
+  //   if (prevProps.name !== this.props.name) {
+  //    this.setState({loading:true})
+  //     fetch(`https://pixabay.com/api/?q=${this.props.name}&page=1&key=${IPA_KEY}&image_type=photo&orientation=horizontal&per_page=12`)
+  //       .then(response => {
+  //         if (response.ok) {
+  //           return response.json()
+  //         }
+  //         return Promise.reject(
+  //           new Error(`Нажаль картинок з вашим пошуком ${this.state.name}, не має`)
+  //         );
+  //       })
+  //       .then(images => this.setState(prevState => ({ items: prevState.items, ...images })))
+  //       // .then(response => response.images)
+  //       //  .then(images => this.setState({ items: [...images] }))
+  //       // .then(images => this.setState({ items: [images] }))
       
-        .catch(error => this.setState({error}))
-        .finally(() => this.setState({ loading: false }))
-    
-    }
-
-  }
+  //       .catch(error => this.setState({error}))
+  //       .finally(() => this.setState({ loading: false }))
+  //   }
+  // }
 
 
 //   ImageGalleryMap = ({images }) => {
@@ -46,9 +44,9 @@ class ImageGallery extends Component {
 //                 key={id}
 //                 webformatURL={webformatURL}
 //                 largeImageURL={largeImageURL} /> )
-//             } 
+//             }
 //       {/* {images.map((image) => {
-//           return <li key={image.id} className={css.ImageGalleryItem }>       
+//           return <li key={image.id} className={css.ImageGalleryItem }>
 //       <img src={image.webformatURL} alt="" large={image.largeImageURL} className={css.ImageGalleryItem } />
 //   </li>})
 //             }  */}
@@ -57,16 +55,19 @@ class ImageGallery extends Component {
 // }
 
   render() {
-    const { items } = this.state;
-    const { loading } = this.state;
-    const { error } = this.state;
+    // const { items } = this.state;
+    // const { loading } = this.state;
+    // const { error } = this.state;
     // const { name } = this.props;
     
-    console.log(items)
-    return ( 
+    // console.log(items)
+    return (
       <div>
-        {error && <div><p>{error.message}</p></div>}
-        {loading && <div>Loading</div>} 
+        <ul className={css.gallery}>
+   
+        </ul>
+        {/* {error && <div><p>{error.message}</p></div>}
+        {loading && <div>Loading</div>}
         {items && <ul className={css.ImageGallery}>
           {items.map(({ id, webformatURL, largeImageURL }) =>
            <ImageGalleryItem
@@ -75,20 +76,16 @@ class ImageGallery extends Component {
               largeImageURL={largeImageURL}
             />)}
 
-      </ul>}
+      </ul>} */}
     </div>
      )
     
-//     <ul class={css.gallery}>
-//            {items && 
-//              <li key={this.state.items.id} class={css.galleryItem}>  
-//        {/* {this.props.name} */}       
-//        <img src={this.state.items.webformatURL} alt="" />
-//            </li>
-//            } 
-// </ul>    
+   
    }
   
 }
+
+
+
 
 export default ImageGallery;
